10352b9ecec643cc817429fd43e15c267b90e7e4
GecaTM <GecaTM@GecaTM-PC.zyxel.com>
2015-01-30 15:13:55 +0200

    21321

---
+++ communication/serverapp/src/main/java/com/sirma/itt/chat/server/ServerApp.java
@@ -0,0 +1,9 @@
+package com.sirma.itt.chat.server;
+
+public class ServerApp {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ communication/serverapp/src/main/java/com/sirma/itt/chat/server/ServerComunicator.java
@@ -0,0 +1,29 @@
+package com.sirma.itt.chat.server;
+
+import com.sirma.itt.comunicator.Communicator;
+import com.sirma.itt.comunicator.Message;
+import com.sirma.itt.comunicator.MessageTransferer;
+
+public class ServerComunicator implements Communicator {
+
+	public void startConection(String ip, int port) {
+
+	}
+
+	public void stopConection() {
+
+	}
+
+	public void addUserSession(MessageTransferer transferer) {
+
+	}
+
+	public void closeUserSession(MessageTransferer transferer) {
+
+	}
+
+	public void processMesage(Message message, MessageTransferer transferer) {
+
+	}
+
+}
