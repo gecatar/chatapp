1ed60a3a62133373f857abc9e0516150de7d1c80
GecaTM <GecaTM@GecaTM-PC.zyxel.com>
2015-01-31 13:40:15 +0200

    21213

--- communication/serverapp/src/main/java/com/sirma/itt/chat/server/ConectionPanel.java
+++ communication/serverapp/src/main/java/com/sirma/itt/chat/server/ConectionPanel.java
@@ -4,7 +4,9 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -19,7 +21,7 @@ import com.sirma.itt.comunicator.ComponentID;
  * @author Evgeni Stefanov
  * 
  */
-public class ConectionPanel extends JPanel {
+public class ConectionPanel extends JFrame {
 
 	private static final String newLine = System.getProperty("line.separator");
 	private final JTextField ipAdress = new JTextField(15);
@@ -32,7 +34,8 @@ public class ConectionPanel extends JPanel {
 	 */
 	public ConectionPanel(ActionListener listener) {
 		this.listener = listener;
-		// setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		JPanel panel = new JPanel();
 		ipAdress.setText("localhost");
 		port.setText("1300");
 		JButton startConection = new JButton("Conect");
@@ -54,17 +57,19 @@ public class ConectionPanel extends JPanel {
 		JScrollPane scrollPane = new JScrollPane(textArea);
 		scrollPane.setMaximumSize(new Dimension(200, 250));
 		// -----------------------------------------------
-		add(ipAdress);
-		add(port);
-		add(startConection);
-		add(stopConection);
-		add(ipLabel);
-		add(ipAdress);
-		add(portLabel);
-		add(port);
-		add(startConection);
-		add(stopConection);
-		add(scrollPane);
+		panel.add(ipAdress);
+		panel.add(port);
+		panel.add(startConection);
+		panel.add(stopConection);
+		panel.add(ipLabel);
+		panel.add(ipAdress);
+		panel.add(portLabel);
+		panel.add(port);
+		panel.add(startConection);
+		panel.add(stopConection);
+		panel.add(scrollPane);
+		add(panel);
+		setVisible(true);
 	}
 
 	/**
